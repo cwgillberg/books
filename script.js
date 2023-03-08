98de674c1f1cde78d4539efb0ef6b4a4ee3e109e
Carl-William Gillberg <cwgillberg@gmail.com>
2023-03-08 15:51:43 +0100

    added donate and initial browse function
    script.js now creates a mock library when the window is loaded
    donate book button shows/hides the form
    browse collection (crudely) displays the mock library

--- script.js
+++ script.js
@@ -3,25 +3,50 @@ let myLibrary = [];
 const donateButton = document.querySelector(".donate");
 const browseButton = document.querySelector(".browse");
 const form = document.querySelector(".form");
+const collection = document.querySelector(".collection");
+
+window.addEventListener('load', () => {
+    mockLibrary();
+})
+
 
 browseButton.addEventListener("click", () => {
-
+    displayLibrary();
 });
 
-donateButton.addEventListener("click", () => {
-    form.style.display = "none";
-    console.log("clicked");
+donateButton.addEventListener('click', () => {
+    if(form.style.display == 'grid') {
+        form.style.display = 'none';
+    } else {
+        form.style.display = 'grid';
+    }
 });
 
-function Book(name, author, pages, read) {
+function mockLibrary() {
+    const book1 = new Book("Meditations", "Marcus Aurelius", 300);
+    const book2 = new Book("Ad urbe condita", "Livy", 2300);
+    const book3 = new Book("De re publica ", "Cicero", 231);
+
+    myLibrary.push(book1);
+    myLibrary.push(book2);
+    myLibrary.push(book3);
+}
+
+function Book(name, author, pages) {
     this.name = name;
     this.author = author;
     this.pages = pages;
-    this.read = read;
 }
 
 function addBookToLibrary() {
 
 }
 
-function displayLibrary() {}
+
+//TODO: Fix table concept etc :)
+function displayLibrary() {
+    for(let i = 0; i < myLibrary.length; i++) {
+        const displayed = document.createTextNode(myLibrary[i].name);
+        collection.appendChild(displayed);
+    }
+}
