8290b3cdbc243f5a623ca08590d3d2a9e0c9b6e8
Carl-William Gillberg <cwgillberg@gmail.com>
2023-03-14 16:11:33 +0100

    added book deposit/withdrawal
    finished addition of books to the library
    finished withdrawal of books from the library
    added speaker, where messages are communicated to the user when depositing / withdrawing a book

--- script.js
+++ script.js
@@ -1,25 +1,65 @@
 let myLibrary = [];
+var browseActive = false;
+var donateActive = false;
 
 const donateButton = document.querySelector(".donate");
 const browseButton = document.querySelector(".browse");
 const form = document.querySelector(".form");
 const collection = document.querySelector(".collection");
+const submitButton = document.querySelector('.submit');
+const speaker = document.querySelector('.speaker');
 
 window.addEventListener('load', () => {
     mockLibrary();
 })
 
-
 browseButton.addEventListener("click", () => {
-    displayLibrary();
+    if(browseActive == false && donateActive == false) {
+        displayLibrary();
+        browseActive = true;
+    } else if(browseActive == false && donateActive == true) {
+        toggleForm('off');
+        donateActive = false;
+
+        displayLibrary();
+        browseActive = true;
+    } else {
+        hideLibrary();
+        browseActive = false;
+    }
 });
 
 donateButton.addEventListener('click', () => {
-    if(form.style.display == 'grid') {
-        form.style.display = 'none';
+    if(donateActive == true) {
+        toggleForm('off')
+        donateActive = false;
+
+    } else if(donateActive == false && browseActive == true) {
+        hideLibrary();
+        browseActive = false;
+
+        toggleForm('on');
+        donateActive = true;
+
     } else {
-        form.style.display = 'grid';
+        toggleForm('on');
+        donateActive = true;
     }
+
+});
+
+submitButton.addEventListener('click', () => {
+    let title = document.querySelector('#title').value;
+    let author = document.querySelector('#author').value;
+    let pages = document.querySelector('#pages').value;
+
+    if(title !== '' && author !== '' && pages !== '') {
+        addBookToLibrary(title, author, pages);
+        document.querySelector('form').reset();  
+        toggleForm('off');
+        speakerTimeOut(`Thank you for submitting <em>${title}</em> to the library!`);
+    } 
+
 });
 
 function mockLibrary() {
@@ -38,20 +78,39 @@ function Book(title, author, pages) {
     this.pages = pages;
 }
 
-function addBookToLibrary() {
-
+function toggleForm(state) {
+    if(state == 'on') {
+        form.style.display = 'grid';
+    } else {
+        form.style.display = 'none';
+    }
 }
 
-/*
-function make card
-    set title -> title
-    set author -> author
-    set pages -> pages
+function addBookToLibrary(title, author, pages) {
+    const book = new Book(title, author, pages);
+    myLibrary.push(book);
+}
 
 
-*/
+function withdraw(title) {
+    let index = 0;
+
+    for(let i = 0; i < myLibrary.length; i++) {
+        if(myLibrary[i].title == title) {
+            index = i;
+        }
+    }
+
+    speakerTimeOut(`You have withdrawn <em>${title}</em>, please return it within a period of 10 days`);
+
+    myLibrary.splice(index, 1);
+
+    hideLibrary();
+    displayLibrary();
+}
 
 function makeCard(title, author, pages) {
+    //card and card sections
     let card = document.createElement('div');
     card.className = 'card';
 
@@ -61,6 +120,7 @@ function makeCard(title, author, pages) {
     let cardRight = document.createElement('div');
     cardRight.className = 'card-right';
 
+    //card elements
     let cardTitle = document.createElement('p');
     cardTitle.style.textDecoration = 'underline';
     cardTitle.innerHTML = title;
@@ -95,10 +155,18 @@ function displayLibrary() {
     for(let i = 0; i < myLibrary.length; i++) {
         collection.appendChild(makeCard(myLibrary[i].title, myLibrary[i].author, myLibrary[i].pages));
     }
-
-   
 }
 
-function withdraw(title) {
+function hideLibrary() {
+    //credit: https://stackoverflow.com/questions/3955229/remove-all-child-elements-of-a-dom-node-in-javascript
+    while(collection.firstChild) {
+        collection.removeChild(collection.lastChild);
+    }
+}
 
+function speakerTimeOut(msg) {
+    speaker.innerHTML = msg;
+    setTimeout(()=>{
+        speaker.innerHTML = '';
+    }, 4000);
 }
